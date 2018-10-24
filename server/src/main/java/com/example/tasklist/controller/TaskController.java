7fc500445a5bbcc703489205612b9bfd61eea178
castro150 <rafaelcarneiroget@hotmail.com>
2018-10-24 17:57:05 -0300

    Allow all cors

--- server/src/main/java/com/example/tasklist/controller/TaskController.java
+++ server/src/main/java/com/example/tasklist/controller/TaskController.java
@@ -9,6 +9,7 @@ import com.example.tasklist.service.TaskService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class TaskController {
